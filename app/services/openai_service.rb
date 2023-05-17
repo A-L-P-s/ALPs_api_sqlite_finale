e79b4fe889d1c39541c1af81d95fb6ba57669c37
Caleb Thomas <cjthomas00@comcast.net>
2023-05-17 11:38:37 -0600

    feat: updated openai_service #get_response.
    Co-authored-by: HuyPhan2025 <huyphan520@gmail.com>
    Co-authored-by: JTaylor28 <jrtaylor0614@gmail.com>
    Co-authored-by: Davejm8 <davidjmarino8@gmail.com>
    Co-authored-by: MelTravelz <meltravelz@gmail.com>

--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -1,52 +1,45 @@
 class OpenaiService
 
-  def self.get_response(info) 
-    payload = {
-      "model": "gpt-3.5-turbo",
-      "messages": [
-        {
-          "role": "user",
-          "content": 
-          "Language: #{Challenge.find(info[:id]).language}
-           Verb: #{Challenge.find(info[:id]).verb}
-           1_sentence: #{Challenge.find(info[:id]).sentences.first.user_sent}
-           1_grammar_point: #{Challenge.find(info[:id]).sentences.first.grammar_point}
-           2_sentence: #{Challenge.find(info[:id]).sentences.second.user_sent}
-           2_grammar_point: #{Challenge.find(info[:id]).sentences.second.grammar_point}
+  # def self.get_response(info) 
+  #   payload = {
+  #     "model": "gpt-3.5-turbo",
+  #     "messages": [
+  #       {
+  #         "role": "user",
+  #         "content": 
+  #         "Language: #{Challenge.find(info[:id]).language}
+  #          Verb: #{Challenge.find(info[:id]).verb}
+  #          1_sentence: #{Challenge.find(info[:id]).sentences.first.user_sent}
+  #          1_grammar_point: #{Challenge.find(info[:id]).sentences.first.grammar_point}
+  #          2_sentence: #{Challenge.find(info[:id]).sentences.second.user_sent}
+  #          2_grammar_point: #{Challenge.find(info[:id]).sentences.second.grammar_point}
            
-           I am trying to learn #{Challenge.find(info[:id]).language}, and I have been given a challenge to write 2 sentences using the verb #{Challenge.find(info[:id]).verb}. I have written this sentence #{Challenge.find(info[:id]).sentences.first.user_sent} trying to use the grammar point: #{Challenge.find(info[:id]).sentences.first.grammar_point}, and I have written this sentence #{Challenge.find(info[:id]).sentences.second.user_sent} trying to use the grammar point: #{Challenge.find(info[:id]).sentences.second.grammar_point}.
+  #          I am trying to learn #{Challenge.find(info[:id]).language}, and I have been given a challenge to write 2 sentences using the verb #{Challenge.find(info[:id]).verb}. I have written this sentence #{Challenge.find(info[:id]).sentences.first.user_sent} trying to use the grammar point: #{Challenge.find(info[:id]).sentences.first.grammar_point}, and I have written this sentence #{Challenge.find(info[:id]).sentences.second.user_sent} trying to use the grammar point: #{Challenge.find(info[:id]).sentences.second.grammar_point}.
 
-           If a sentence is correct, please return the same sentence. If a sentence is incorrect, please return the sentence with necessary corrections to that given sentence. Make sure to keep the provided verb and grammar point in your revision.
+  #          If a sentence is correct, please return the same sentence. If a sentence is incorrect, please return the sentence with necessary corrections to that given sentence. Make sure to keep the provided verb and grammar point in your revision.
 
-           Please format the output as" 
+  #          Please format the output as" 
+  #       }
+  #     ],
+  #     "temperature": 0.9,
+  #     "max_tokens": 600
+  #   }
+  #   get_url(payload)
+  # end
+ def self.get_response(info)
+  payload = { 
+    "model": "gpt-3.5-turbo",
+    "messages": [
+        {
+            "role": "user",
+            "content": "I am trying to learn #{Challenge.find(info[:id]).language}. This sentence uses the grammar #{Challenge.find(info[:id]).sentences.first.grammar_point} and the verb #{Challenge.find(info[:id]).verb}: '#{Challenge.find(info[:id]).sentences.first.user_sent}'. Correct this sentence if it's wrong and explain what you changed. This sentence uses the grammar #{Challenge.find(info[:id]).sentences.second.grammar_point} and the verb #{Challenge.find(info[:id]).verb}: '#{Challenge.find(info[:id]).sentences.second.user_sent}'. Correct this sentence if it's wrong and explain what you changed. Format the output as a Ruby hash. Example: { sentence_1: 'your response', sentence_2: 'your response', explanation_1: 'your response', explanation_2: 'your response' }"
         }
       ],
-      "temperature": 0.9,
-      "max_tokens": 100
-    }
-    get_url(payload)
-  end
-#  def generate_playlists(info) 
-#     payload = {
-#       "model": "gpt-3.5-turbo",
-#       "messages": [
-#         {
-#           "role": "user",
-#           "content": 
-#           "Character: #{Character.find(info[:character_id]).name}
-#           Alignment: #{Character.find(info[:character_id]).alignment}
-#           Theme: #{Theme.find(info[:theme_id]).name}
-#           Style of Music: #{info[:genre]}
-
-#           I am #{Character.find(info[:character_id]).name}. I am a #{Character.find(info[:character_id]).alignment} person on the DnD alignment chart. Generate a playlist that a person like me would listen to. Only pick one song referencing me or my world, make the rest things I would listen to.  I would like these to be real songs that are on Spotify. The phrase NEXT_SONG should be between each song. 
-#           Please format the output as an array of strings in Ruby, like this and without any text or numbers before or after in your response: Song1 NEXT_SONG Song2 NEXT_SONG Song3 NEXT_SONG Song4 NEXT_SONG Song5 NEXT_SONG Song6 NEXT_SONG Song7. Exactly 10 songs. No artist names."
-#         }
-#       ],
-#       "temperature": 0.7,
-#       "max_tokens": 1000
-#     }
-#     get_url(payload)
-#   end
+    "temperature": 0.9,
+    "max_tokens": 600
+  }
+  get_url(payload)
+ end
 
   private
 
