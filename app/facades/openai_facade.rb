c2dd8965315307a59806352c7ae5ba13e50e6a0f
Caleb Thomas <cjthomas00@comcast.net>
2023-05-17 20:22:29 -0600

    feat: make changes to openai service.

--- app/facades/openai_facade.rb
+++ app/facades/openai_facade.rb
@@ -7,6 +7,9 @@ class OpenaiFacade
 
   def self.sort_response(response)
     manipulated_response = response[:choices].first[:message][:content]
+    manipulated_response = manipulated_response[1..-2]
+    manipulated_response = manipulated_response.split(". ")
+    manipulated_response = manipulated_response.map(&:strip)
     require 'pry'; binding.pry
   end
 end
--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -6,13 +6,18 @@ class OpenaiService
       "messages": [
           {
               "role": "user",
-              "content": "I am trying to learn #{Challenge.find(info[:id]).language}. This sentence uses the grammar #{Challenge.find(info[:id]).sentences.first.grammar_point} and the verb #{Challenge.find(info[:id]).verb}: '#{Challenge.find(info[:id]).sentences.first.user_sent}'. Correct this sentence if it's wrong and explain what you changed. This sentence uses the grammar #{Challenge.find(info[:id]).sentences.second.grammar_point} and the verb #{Challenge.find(info[:id]).verb}: '#{Challenge.find(info[:id]).sentences.second.user_sent}'. Correct this sentence if it's wrong and explain what you changed. Format the output as a Ruby array of strings. Example: ["sentence_1: your response", "sentence_2: 'your response'", "explanation_1: 'your response'", "explanation_2: 'your response'"]"
+              "content": "I am trying to learn #{Challenge.find(info[:id]).language}. This sentence uses the grammar #{Challenge.find(info[:id]).sentences.first.grammar_point} and the verb #{Challenge.find(info[:id]).verb}: '#{Challenge.find(info[:id]).sentences.first.user_sent}'. Correct this sentence if it's wrong and explain what you changed. This sentence uses the grammar #{Challenge.find(info[:id]).sentences.second.grammar_point} and the verb #{Challenge.find(info[:id]).verb}: '#{Challenge.find(info[:id]).sentences.second.user_sent}'. Correct this sentence if it's wrong and explain what you changed. Format the output as multiple Ruby arrays of strings. Example: [sentence_1: your response] [sentence_2: your response] [explanation_1: your response] [explanation_2: your response]"
           }
         ],
       "temperature": 0.9,
       "max_tokens": 600
     }
-    get_url(payload)
+    response = get_url(payload)
+    require 'pry'; binding.pry
+    content = response[:choices].first[:message][:content]
+    content = content[1..-2] # Remove the curly braces at the start and end 
+    content = content.split("&") # Split the string into an array based on commas
+    content = content.map(&:strip) # Remove leading and trailing whitespace from each array element
   end
 
   private
