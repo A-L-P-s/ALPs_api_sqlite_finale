fe3ec4744a3b9b1a34b6a02a3aab53f07c05f6da
Melony Franchini <meltravelz@gmail.com>
2023-05-18 00:25:38 -0600

    feat: Write start of UsersChallenges#create facade test & action

--- app/controllers/api/v1/users/challenges_controller.rb
+++ app/controllers/api/v1/users/challenges_controller.rb
@@ -1,18 +1,34 @@
 class Api::V1::Users::ChallengesController < ApplicationController
   before_action :check_challenge, only: [:destroy]
 
+  def new
+    prompt_info = UnsplashFacade.new(params).prompt_data
+    render json: ChallengeSerializer.new(prompt_info)
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    new_challenge = user.challenges.create(user_id: params[:user_id], language: params[:language], verb: params[:verb], eng_verb: params[:eng_verb], image_url: params[:image_url], image_alt_text: params[:image_alt_text])
+  
+    params[:sentences].each do |sentence|
+      new_challenge.sentences.create(grammar_point: sentence[:grammar_point], eng_grammar_point: sentence[:eng_grammar_point], user_sent: sentence[:user_sent])
+    end
+    
+    require 'pry'; binding.pry
+    #Here we call the facade method
+    # this method/helpers will call the OpenAI service, clean the data, update the two new 'Sentences', and return an object as per the JSON contract
+    # it all might look like this: 
+
+    # updated_challenge = OpenaiFacade.method_name_here
+    # render json: ChallengeSerializer.new(updated_challenge)
+  end
+
   def show
     render json: ChallengeSerializer.new(Challenge.find(params[:id]))
   end
-  
+
   def destroy
     @challenge.destroy
-  end
-
-  def new
-    prompt_info = UnsplashFacade.new(params).prompt_data
-
-    render json: ChallengeSerializer.new(prompt_info)
   end
 
   private
