beadb3fe5279da6ff37c9ca61c8f8a2a8292f3d2
Melony Franchini <meltravelz@gmail.com>
2023-05-20 17:35:36 -0600

    refactor: update serializer to match JSON contract

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,18 @@
 class UserSerializer
   include JSONAPI::Serializer
-  set_type :user
-  attributes :name, :preferred_lang, :challenges
+  attributes :name, :preferred_lang
+
+  attributes :challenges do |serializer|
+    serializer.challenges.map do |challenge|
+      {
+        challenge_id: challenge.id,
+        language: challenge.language,
+        verb: challenge.verb,
+        eng_verb: challenge.eng_verb,
+        image_url: challenge.image_url,
+        image_alt_text: challenge.image_alt_text,
+        created_at: challenge.created_at.strftime("%m/%e/%Y")
+      }
+    end
+  end
 end
