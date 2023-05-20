dcb0fcfd5de9e1074754ca38915021743ee9b294
Melony Franchini <meltravelz@gmail.com>
2023-05-20 17:37:24 -0600

    fix: remove unecessary lines

--- app/serializers/challenge_serializer.rb
+++ app/serializers/challenge_serializer.rb
@@ -2,10 +2,6 @@ class ChallengeSerializer
   include JSONAPI::Serializer
   attributes :user_id, :language, :verb, :eng_verb, :image_url, :image_alt_text
 
-  attributes :user_id do |object|
-    object.to_s
-  end
-
   attributes :created_at do |object|
     object.created_at.strftime("%m/%e/%Y")
   end
@@ -13,8 +9,8 @@ class ChallengeSerializer
   attributes :sentences do |serializer|
     serializer.sentences.map do |sentence|
       {
-        id: sentence.id.to_s,
-        grammar_points: sentence.grammar_point,
+        id: sentence.id,
+        grammar_point: sentence.grammar_point,
         eng_grammar_point: sentence.eng_grammar_point,
         user_sent: sentence.user_sent,
         ai_sent: sentence.ai_sent,
